fa6355ff03928401b5dd1e7bb1504f63914bda60
yuvansundrani <yuvan.sundrani@gmail.com>
2022-10-07 22:39:21 -0400

    lab 5

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -1,11 +1,6 @@
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
-
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something ****************************************
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
 
@@ -20,7 +15,6 @@ async function mainEvent() { // the async keyword means we can make API requests
     // this is the preferred way to handle form data in JS in 2022
     const formData = new FormData(submitEvent.target); // get the data from the listener target
     const formProps = Object.fromEntries(formData); // Turn it into an object
-
     // You can also access all forms in a document by using the document.forms collection
     // But this will retrieve ALL forms, not just the one that "heard" a submit event - less good
 
@@ -35,13 +29,12 @@ async function mainEvent() { // the async keyword means we can make API requests
       // It does not include any of your form values, though
     */
 
-    const results = await fetch('/api/foodServicePG');
+    const fetchQuery = new URLSearchParams(formProps);
+    const results = await fetch(`/api/foodServicePG?${fetchQuery}`); 
     /*
    ## Get request with query parameters
 
-      const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
-
-      The above request uses "string interpolation" to include an encoded version of your form values
+    The above request uses "string interpolation" to include an encoded version of your form values
       It works because it has a ? in the string
       Replace line 37 with it, and try it with a / instead to see what your server console says
 
@@ -63,4 +56,4 @@ async function mainEvent() { // the async keyword means we can make API requests
   The use of the async keyword means we can "await" events before continuing in our scripts
   In this case, we load some data when the form has submitted
 */
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
